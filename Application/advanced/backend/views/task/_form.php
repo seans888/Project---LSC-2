8732e7afef4b9d7ba1462dd6ed59f70e2fbf1e1a
jcheramia7 <johannaheramia@yahoo.com>
2016-08-07 17:57:46 +0800

    dependent dropdownlist

--- Application/advanced/backend/views/task/_form.php
+++ Application/advanced/backend/views/task/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Course;
+use common\models\Student;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Task */
@@ -16,9 +19,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'category')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'course_id')->textInput() ?>
+	<?= $form->field($model, 'course_id')->dropDownList(
+		ArrayHelper::map( Course::find()->all(), 'id', 'course_name'),
+		[
+			'prompt'=>'Select Course',
+			//'onchange'=>'
+			//	$.post("index.php?r=work-orders/lists&id='.'"+$(this).val(), function(data){
+			//		$("select#models-contact").html(data);
+			//		});'
+		]); ?>
 
-    <?= $form->field($model, 'student_id')->textInput() ?>
+	<?= $form->field($model, 'student_id')->dropDownList(
+		ArrayHelper::map( Student::find()->all(), 'id', 'username'),
+		[
+			'prompt'=>'Select Student',
+			//'onchange'=>'
+			//	$.post("index.php?r=work-orders/lists&id='.'"+$(this).val(), function(data){
+			//		$("select#models-contact").html(data);
+			//		});'
+		]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
