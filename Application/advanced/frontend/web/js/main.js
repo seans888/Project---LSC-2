28ebe5f6f74c021f451421bd90a36267356b3151
jcheramia7 <johannaheramia@yahoo.com>
2016-08-08 15:35:28 +0800

    delete .gitignore

---
+++ Application/advanced/frontend/web/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	//get the click of the create button
+	$('#modalButton').click(function(){
+		$('#modal').modal('show')
+			.find('#modalContent')
+			.load($(this).attr('value'));
+		});
+	});
