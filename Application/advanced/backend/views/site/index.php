cda653f8370adacbaf5db61ea93fd7c8d29fcacb
jcheramia7 <johannaheramia@yahoo.com>
2016-08-11 15:57:47 +0800

    update design

--- Application/advanced/backend/views/site/index.php
+++ Application/advanced/backend/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <!--<h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">You have successfully created your Yii-powered application.</p>-->
 
         <p><a class="btn btn-lg btn-success" href="http://localhost/advanced/backend/web/index.php?r=course">Course</a></p>
 		<p><a class="btn btn-lg btn-success" href="http://localhost/advanced/backend/web/index.php?r=student">Student</a></p>
--- Application/advanced/frontend/config/main.php
+++ Application/advanced/frontend/config/main.php
@@ -52,6 +52,7 @@ return [
             ],
         ],
         */
+
     ],
     'params' => $params,
 ];
