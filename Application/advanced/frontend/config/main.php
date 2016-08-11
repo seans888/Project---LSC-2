3f5fcafbf68583a42a7b0e4afa957fd7d5159633
jcheramia7 <johannaheramia@yahoo.com>
2016-08-11 13:49:14 +0800

    update

--- Application/advanced/frontend/config/main.php
+++ Application/advanced/frontend/config/main.php
@@ -4,7 +4,7 @@ $params = array_merge(
     require(__DIR__ . '/../../common/config/params-local.php'),
     require(__DIR__ . '/params.php'),
     require(__DIR__ . '/params-local.php')
-);
+);  
 
 return [
     'id' => 'app-frontend',
