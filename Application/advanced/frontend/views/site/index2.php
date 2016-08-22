d96f787de07896f8f706b42a1fbe13d80bbcbda1
jgtadeo <renzotadeo26@gmail.com>
2016-08-22 16:04:50 +0800

    updated index

--- Application/advanced/frontend/views/site/index2.php
+++ Application/advanced/frontend/views/site/index2.php
@@ -7,6 +7,7 @@ $this->title = 'LSC-LMS';
 <div class="site-index">
 
     <div class="jumbotron">
+	<img src="http://i1044.photobucket.com/albums/b444/jgtadeo/Loyola-Student-Center%201_zpsbbvbqnsv.png" style="width:250px;height:150px;"><br><br>
         <h1>WELCOME </h1>
 
         <p class="lead">Loyola Student Center</p>
