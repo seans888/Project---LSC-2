bb684f539584685aebd462dcef928cd7f174c01f
Jose Lorenzo Tadeo <renzotadeo26@gmail.com>
2016-08-31 00:34:14 +0800

    front end index watermark

--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -7,9 +7,12 @@ $this->title = 'Home';
 <body>
 <div class="sidenav">
 	<a href="#">My Profile</a>
-	<a href="#">Task</a>
+	<a href="#">Course</a>
 	<a href="#">Message</a>
 	<a href="#">Calendar</a>
+</div>
+<div class="logo">
+<img src="http://i1044.photobucket.com/albums/b444/jgtadeo/try_zpse1sdxrvq.png" style="width:700px;height:500px;">
 </div>
 
 </body>
