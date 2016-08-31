37577e685634cc173513902ad9c51ed1c0f829ed
jgtadeo <renzotadeo26@gmail.com>
2016-09-01 01:03:16 +0800

    added layout

--- Application/advanced/backend/views/layouts/index2Layout.php
+++ Application/advanced/backend/views/layouts/index2Layout.php
@@ -25,14 +25,8 @@ IndexAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div class="sidenav">
-	<a href="http://localhost/advanced/backend/web/index.php?r=task" target = "_self">Task</a>
-	<a href="http://localhost/advanced/backend/web/index.php?r=course">Course</a>
-	<a href="http://localhost/advanced/backend/web/index.php?r=student">Student</a>
-	<a href="http://localhost/advanced/backend/web/index.php?r=event">Calendar</a>
-	<a href="http://localhost/advanced/backend/web/index.php?r=grade">Grade</a>
-	<a href="http://localhost/advanced/backend/web/index.php?r=message">Message</a>
-</div>
+
+
 <div class="wrap">
     <?php
     NavBar::begin([
