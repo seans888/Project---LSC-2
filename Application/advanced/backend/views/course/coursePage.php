536a808726dc9177dab848f785a43fd1b8fb98f0
jcheramia7 <johannaheramia@yahoo.com>
2016-10-25 05:43:40 +0800

    fix some error

--- Application/advanced/backend/views/course/coursePage.php
+++ Application/advanced/backend/views/course/coursePage.php
@@ -10,7 +10,14 @@ $this->title = 'COURSE';
 
 ?>
 <div class="course-view">
-
+<br><br>
+	<div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="icon fa fa-check"></i>
+                Course successfully created
+              </div>
+			  
+			  <a class="btn btn-primary"href="index.php?r=task/create"> CREATE TASK</a>
     
 
 </div>
