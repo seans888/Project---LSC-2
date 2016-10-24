c90c8d1ba10b4963e46d89eca632307abd75c9bb
jcheramia7 <johannaheramia@yahoo.com>
2016-10-24 16:02:42 +0800

    update backend design

--- Application/advanced/backend/views/layouts/main.php
+++ Application/advanced/backend/views/layouts/main.php
@@ -27,7 +27,7 @@ DashboardAsset::register($this);
 <div class="wrapper">
     <header class="main-header">
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="index.php" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>A</b>LT</span>
             <!-- logo for regular state and mobile devices -->
@@ -306,25 +306,25 @@ DashboardAsset::register($this);
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu">
                 <li class="header">MAIN NAVIGATION</li>
-                <li class="active treeview">
+               <!-- <li class="active treeview">
                     <a href="#">
                         <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                             <i class="fa fa-angle-left pull-right"></i>
+                         </span>
                     </a>
                     <ul class="treeview-menu">
                         <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
                         <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
                     </ul>
-                </li>
-                <li class="treeview">
+                </li>-->
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-files-o"></i>
                         <span>Layout Options</span>
                         <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
-            </span>
+                            <span class="label label-primary pull-right">4</span>
+                         </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
@@ -332,22 +332,52 @@ DashboardAsset::register($this);
                         <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
                         <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
                     </ul>
-                </li>
+                </li>-->
+
                 <li>
-                    <a href="pages/widgets.html">
-                        <i class="fa fa-th"></i> <span>Widgets</span>
-                        <span class="pull-right-container">
-              <small class="label pull-right bg-green">new</small>
-            </span>
+                    <a href="index.php?r=student">
+                        <i class="fa fa-users"></i> <span>Students</span>
+                        <!--<span class="pull-right-container">
+                            <small class="label pull-right bg-green">View</small>
+                        </span>-->
                     </a>
                 </li>
-                <li class="treeview">
+
+                <li>
+                    <a href="index.php?r=employee">
+                        <i class="fa fa-users"></i> <span>Employees</span>
+                        <!--<span class="pull-right-container">
+                            <small class="label pull-right bg-green">View</small>
+                        </span>-->
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?r=ann-calendar">
+                        <i class="fa fa-calendar-check-o"></i> <span>Calendar</span>
+                        <!--<span class="pull-right-container">
+                            <small class="label pull-right bg-green">View</small>
+                        </span>-->
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php?r=course">
+                        <i class="fa fa-book"></i> <span>Courses</span>
+                        <!--<span class="pull-right-container">
+                            <small class="label pull-right bg-green">View</small>
+                        </span>-->
+                    </a>
+                </li>
+
+
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-pie-chart"></i>
                         <span>Charts</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
@@ -355,14 +385,14 @@ DashboardAsset::register($this);
                         <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
                         <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
                     </ul>
-                </li>
-                <li class="treeview">
+                </li>-->
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-laptop"></i>
                         <span>UI Elements</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
@@ -372,57 +402,57 @@ DashboardAsset::register($this);
                         <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
                         <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
                     </ul>
-                </li>
-                <li class="treeview">
+                </li>-->
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-edit"></i> <span>Forms</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
                         <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
                         <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
                     </ul>
-                </li>
-                <li class="treeview">
+                </li>-->
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-table"></i> <span>Tables</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
                         <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
                     </ul>
-                </li>
-                <li>
+                </li>-->
+                <!--<li>
                     <a href="pages/calendar.html">
                         <i class="fa fa-calendar"></i> <span>Calendar</span>
                         <span class="pull-right-container">
-              <small class="label pull-right bg-red">3</small>
-              <small class="label pull-right bg-blue">17</small>
-            </span>
+                            <small class="label pull-right bg-red">3</small>
+                            <small class="label pull-right bg-blue">17</small>
+                        </span>
                     </a>
-                </li>
-                <li>
+                </li>-->
+                <!--<li>
                     <a href="pages/mailbox/mailbox.html">
                         <i class="fa fa-envelope"></i> <span>Mailbox</span>
                         <span class="pull-right-container">
-              <small class="label pull-right bg-yellow">12</small>
-              <small class="label pull-right bg-green">16</small>
-              <small class="label pull-right bg-red">5</small>
-            </span>
+                            <small class="label pull-right bg-yellow">12</small>
+                            <small class="label pull-right bg-green">16</small>
+                            <small class="label pull-right bg-red">5</small>
+                        </span>
                     </a>
-                </li>
-                <li class="treeview">
+                </li>-->
+                <!--<li class="treeview">
                     <a href="#">
                         <i class="fa fa-folder"></i> <span>Examples</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
@@ -435,29 +465,29 @@ DashboardAsset::register($this);
                         <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
                         <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
                     </ul>
-                </li>
-                <li class="treeview">
+                </li>-->
+               <!-- <li class="treeview">
                     <a href="#">
                         <i class="fa fa-share"></i> <span>Multilevel</span>
                         <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
                     </a>
                     <ul class="treeview-menu">
                         <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
                         <li>
                             <a href="#"><i class="fa fa-circle-o"></i> Level One
                                 <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
                             </a>
                             <ul class="treeview-menu">
                                 <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
                                 <li>
                                     <a href="#"><i class="fa fa-circle-o"></i> Level Two
                                         <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
+                                            <i class="fa fa-angle-left pull-right"></i>
+                                        </span>
                                     </a>
                                     <ul class="treeview-menu">
                                         <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
@@ -468,12 +498,12 @@ DashboardAsset::register($this);
                         </li>
                         <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
                     </ul>
-                </li>
-                <li><a href="documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
-                <li class="header">LABELS</li>
-                <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-                <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-                <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+                </li>-->
+                <!--<li><a href="documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>-->
+                <!--<li class="header">LABELS</li>
+                    <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>-->
             </ul>
         </section>
         <!-- /.sidebar -->
@@ -483,10 +513,10 @@ DashboardAsset::register($this);
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <h1>
+            <!--<h1>
                 Dashboard
                 <small>Control panel</small>
-            </h1>
+            </h1>-->
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
                 <li class="active">Dashboard</li>
