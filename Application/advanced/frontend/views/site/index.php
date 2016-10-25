ce5245e2767b158ee472a21be916edeb80b9406a
jcheramia7 <johannaheramia@yahoo.com>
2016-10-25 22:53:19 +0800

    update frontend design (index)

--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -5,83 +5,76 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
     <div class="jumbotron">
         <div  class="tag-line" >
-         <div class="container">
-           <div class="row  text-center" >
-           
-               <div class="col-lg-12  col-md-12 col-sm-12">
-			   
-                <!--LOGO-->
-              <div class="span3"><a class="brand" href="#"><img src="img/logo.png"/></a></div>
-              <!-- /LOGO -->
-        <h2 data-scroll-reveal="enter from the bottom after 0.1s" ><i class="fa fa-circle-o-notch"></i> LOYOLA STUDENT CENTER<br>LEARNING MANAGEMENT SYSTEM <i class="fa fa-circle-o-notch"></i> </h2>
+            <div class="container">
+                <div class="row  text-center">
+                    <div class="col-lg-12  col-md-12 col-sm-12">
+                        <!--LOGO-->
+                        <div class="span3">
+                            <a class="brand" href="#">
+                                <img src="http://i1044.photobucket.com/albums/b444/jgtadeo/Loyola-Student-Center%201_zpsbbvbqnsv.png" width="100" height="60"/>
+                            </a>
+                        </div>
+                        <!-- /LOGO -->
+                        <h2 data-scroll-reveal="enter from the bottom after 0.1s" ><i class="fa fa-circle-o-notch"></i> LOYOLA STUDENT CENTER<br>LEARNING MANAGEMENT SYSTEM <i class="fa fa-circle-o-notch"></i> </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br><br><br><br><br>
+        <!--HOME SECTION TAG LINE END-->
+        <div id="features-sec" class="container set-pad" >
+            <div class="row text-center">
+                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                </div>
+            </div>
+            <!--/.HEADER LINE END-->
+            <div class="row" >
+                <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
+                    <div class="about-div">
+                        <span class="glyphicons glyphicons-calendar"></span>
+                        <div class="span3">
+                            <a class="brand" href="#"><img src="img/logo.png"/></a>
+                        </div>
+                        <h3>CALENDAR</h3>
+                        <hr/>
+                        <hr/>
+                        <p>
+                            This is where you can see your schedule of your classes and schedule of your tasks.
+                        </p>
+                        <a href="#" class="btn btn-info btn-set">CALENDAR</a>
+                   </div>
+                </div>
+                <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
+                    <div class="about-div">
+                       <div class="span3">
+                           <a class="brand" href="#"><img src="img/logo.png"/></a>
+                       </div>
+                       <i class="fa fa-bolt fa-4x icon-round-border" ></i>
+                       <h3>TASK</h3>
+                       <hr/>
+                       <hr/>
+                       <p>
+                           This is where you can take your exercises, assignments or quizzes.
+                       </p>
+                       <a href="#" class="btn btn-info btn-set"  >TASK</a>
                    </div>
                </div>
-             </div>
-        
-    </div>
-	<br><br><br><br><br><br><br><br>
-    <!--HOME SECTION TAG LINE END-->   
-         <div id="features-sec" class="container set-pad" >
-             <div class="row text-center">
-                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
-                     
-                    
-                 </div>
-
-             </div>
-             <!--/.HEADER LINE END-->
-
-
-           <div class="row" >
-           
-               
-                 <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
-                     <div class="about-div">
-                     <i class="fa fa-paper-plane-o fa-4x icon-round-border" ></i>
-					 <div class="span3"><a class="brand" href="#"><img src="img/logo.png"/></a></div>
-                   <h3 >CALENDAR</h3>
-                 <hr />
-                       <hr />
-                   <p >
-                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo . 
-                       
-                   </p>
-               <a href="#" class="btn btn-info btn-set"  >CALENDAR</a>
-                </div>
-                   </div>
-                   <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
-                     <div class="about-div">
-					 <div class="span3"><a class="brand" href="#"><img src="img/logo.png"/></a></div>
-                     <i class="fa fa-bolt fa-4x icon-round-border" ></i>
-                   <h3 >TASK</h3>
-                 <hr />
-                       <hr />
-                   <p >
-                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo . 
-                       
-                   </p>
-               <a href="#" class="btn btn-info btn-set"  >TASK</a>
-                </div>
-                   </div>
-<div class="span3"><a class="brand" href="#"><img src="img/logo.png"/></a></div>
-                   <h3 >SCORE SHEET</h3>
-                 <hr />
-                       <hr />
-                   <p >
-                       Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo . 
-                       
-                   </p>
+               <div class="span3">
+                   <a class="brand" href="#"><img src="img/logo.png"/></a>
+               </div>
+               <h3>SCORE SHEET</h3>
+               <hr/>
+               <hr/>
+               <p>
+                   This is where you can see all of your scores in your exercises, assignments or quizzes.
+               </p>
                <a href="#" class="btn btn-info btn-set"  >SCORE</a>
-                </div>
-                   </div>
-               </div>
-             </div>
+           </div>
+         </div>
+    </div>
+</div>
       
 
 
