1ccf2a2b12cb9625935e2d3b48517caccfb48977
jcheramia7 <johannaheramia@yahoo.com>
2016-10-27 22:38:10 +0800

    update frontend design

--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -6,13 +6,13 @@ $this->title = 'LSC LMS';
 ?>
   
 <div class="site-index">
-    <div class="jumbotron">
+	<div class="jumbotron">
         <div  class="tag-line" >
             <div class="container">
                 <div class="row  text-center">
                     <div class="col-lg-12  col-md-12 col-sm-12">
                         <!--LOGO-->
-                        <div class="span3">
+						<div class="span3">
                             <a class="brand" href="#">
                                 <img src="http://i1044.photobucket.com/albums/b444/jgtadeo/Loyola-Student-Center%201_zpsbbvbqnsv.png" width="100" height="60"/>
                             </a>
@@ -20,131 +20,82 @@ $this->title = 'LSC LMS';
                         <!-- /LOGO -->
                         <h2 data-scroll-reveal="enter from the bottom after 0.1s" >
                             LOYOLA STUDENT CENTER<br/>
-                            <i class="fa fa-mortar-board fa-lg"></i>  Learning Management System  <i class="fa fa-mortar-board fa-lg"></i> </h2>
-                    </div>
+							<i class="fa fa-mortar-board fa-lg"></i>  Learning Management System  <i class="fa fa-mortar-board fa-lg"></i> </h2>
+					</div>
                 </div>
             </div>
         </div>
         <br><br><br>
         <!--HOME SECTION TAG LINE END-->
-        <div id="features-sec" class="container set-pad" >
-            <div class="row text-center">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2"></div>
+		<div id="features-sec" class="container set-pad" >
+			<div class="row text-center">
+				<div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2"></div>
             </div>
             <!--/.HEADER LINE END-->
-            <div class="row" >
-             <!--   <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.4s">
-                    <div class="about-div">
-                        <i class="fa fa-calendar fa-lg" ></i>
-                        <h3>CALENDAR</h3>
-                        <hr/>
-                        <hr/>
-                        <p>
-                            This is where you can see your schedule of your classes and schedule of your tasks.
-                        </p>
-                        <a href="#" class="btn btn-info btn-set">CALENDAR</a>
-                    </div>
-                </div>
-                <div class="col-lg-4  col-md-4 col-sm-4" data-scroll-reveal="enter from the bottom after 0.5s">
-                    <div class="about-div">
-                        <!--<i class="fa fa-bolt fa-4x icon-round-border" ></i>-->
-                <!--        <i class="fa fa-tasks fa-lg" aria-hidden="true"></i>
-                        <h3>TASK</h3>
-                        <hr/>
-                        <hr/>
-                        <p>
-                            This is where you can take your exercises, assignments or quizzes.
-                        </p>
-                        <a href="#" class="btn btn-info btn-set"  >TASK</a>
-                    </div>
-                </div>
-                 
-
-                <!--<section class="module col-md-3 col-sm-6">
-                <div class="module-content">
-                    <!--<img src="#"/>-->
-                <!--<div class="module-header"></div>
-                <div class="module-details text-center">
-                    <h3>SCORE SHEET</h3>
-                    <p class="score">
-                        This is where you can see all scores in your exercises, assignments or quizzes.
-                    </p>
-                </div>
-            </div>-->
-                <!--</section>-->
-
-          <!--      <h3>SCORE SHEET</h3>
-                <hr/>
-                <hr/>
-                <p>
-                    This is where you can see all of your scores in your exercises, assignments or quizzes.
-                </p>
-                <a href="#" class="btn btn-info btn-set"  >SCORE</a>
-            </div>
-        </div>
-
-				
-
-        <!-- END -->
-    </div>
+			<div class="row" >
+				<hr>
+				<section class="module-body container-fluid row col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+					<!--CALENDAR[-->
+					<section class="module col-md-3 col-sm-6">
+						<a href="calendar.php">
+							<div class="module-content">
+								<i class="fa fa-calendar fa-lg" ></i>
+								<div class="module-header"></div>
+								<div class="module-details text-center">
+									<center><h3>CALENDAR</h3>
+										<h4>Calendar viewer provides....</h4></center>
+									<p></p>
+								</div>
+							</div>
+						</a>
+					</section>
+					<!--COURSE-->
+					<section class="module col-md-3 col-sm-6">
+						<a href="course.php">
+							<div class="module-content">
+								<i class="fa fa-calendar fa-lg" ></i>
+								<div class="module-header"></div>
+								<div class="module-details text-center">
+									<h3>COURSE</h3>
+									<h4>Course viewer provides....</h4>
+									<p></p>
+								</div>
+							</div>
+						</a>
+					</section>
+					<!--GRADES-->
+					<section class="module col-md-3 col-sm-6">
+						<a href="grades.php">
+							<div class="module-content">
+								<i class="fa fa-tasks fa-lg" aria-hidden="true"></i>
+								<div class="module-header"></div>
+								<div class="module-details text-center">
+									<h3>GRADES</h3>
+									<h4>Grades viewer provides....</h4>
+									<p></p>
+								</div>
+							</div>
+						</a>
+					</section>
+					<!--ATTENDANCE-->
+					<section class="module col-md-3 col-sm-6">
+						<a href="attendance.php">
+							<div class="module-content">
+								<i class="fa fa-calendar fa-lg" ></i>
+								<div class="module-header"></div>
+								<div class="module-details text-center">
+									<h3>ATTENDANCE</h3>
+									<h4>Attendance viewer provides....</h4>
+									<p></p>
+								</div>
+							</div>
+						</a>
+					</section>
+				</section>
+			</div>
+		</div>
+	</div>
 </div>
-	<section class="module-body container-fluid row col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-	<!--CALENDAR[-->
-	<section class="module col-md-3 col-sm-6">
-		<a href="calendar.php">
-			<div class="module-content">
-				<i class="fa fa-calendar fa-lg" ></i>
-				<div class="module-header"></div>
-				<div class="module-details text-center">
-					<center><h3>CALENDAR</h3>
-					<h4>Calendar viewer provides....</h4></center>
-					<p></p>
-				</div>
-			</div>
-		</a>
-	</section>
-	<!--COURSE-->
-	<section class="module col-md-3 col-sm-6">
-		<a href="course.php">
-			<div class="module-content">
-				<img class="move-me" id="badge" alt="" src="images/course.png"/>
-				<div class="module-header"></div>
-				<div class="module-details text-center">
-					<h3>COURSE</h3>
-					<h4>Course viewer provides....</h4>
-					<p></p>
-				</div>
-			</div>
-		</a>
-	</section>
-	<!--GRADES-->
-	<section class="module col-md-3 col-sm-6">
-		<a href="grades.php">
-			<div class="module-content">
-				<i class="fa fa-tasks fa-lg" aria-hidden="true"></i>
-				<div class="module-header"></div>
-				<div class="module-details text-center">
-					<h3>GRADES</h3>
-					<h4>Grades viewer provides....</h4>
-					<p></p>
-				</div>
-			</div>
-		</a>
-	</section>
-	<!--ATTENDANCE-->
-	<section class="module col-md-3 col-sm-6">
-		<a href="attendance.php">
-			<div class="module-content">
-				<img class="move-me" id="badge" alt="" src="images/attendance.png"/>
-				<div class="module-header"></div>
-				<div class="module-details text-center">
-					<h3>ATTENDANCE</h3>
-					<h4>Attendance viewer provides....</h4>
-					<p></p>
-				</div>
-			</div>
-		</a>
-	</section>
 	
 				
 	
