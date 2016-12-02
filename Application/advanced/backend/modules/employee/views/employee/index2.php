b7b65bcb4936daaab3f5efc7957aa91b685ad9d9
jcheramia7 <johannaheramia@yahoo.com>
2016-12-02 19:44:47 +0800

    updated the index for profile module

--- Application/advanced/backend/modules/employee/views/employee/index2.php
+++ Application/advanced/backend/modules/employee/views/employee/index2.php
@@ -5,16 +5,13 @@
 $this->title = 'employee';
 ?>
 
-<div class="employee-default-index">
+<div class="employee-default-index"><br><br><br>
 
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <center><h3><b>First Name:</b> <?php echo Yii::$app->user->identity->first_name ?></h3>
+    <h3><b>Middle Name:</b> <?php echo Yii::$app->user->identity->middle_name ?></h3>
+    <h3><b>Last Name:</b> <?php echo Yii::$app->user->identity->last_name ?></h3>
+    <h3><b>Email: </b> <?php echo Yii::$app->user->identity->email ?></h3>
+    <h3><b>Home address:</b> <?php echo Yii::$app->user->identity->home_add ?></h3>
+    <h3><b>Contact number:</b> </h3><br><br><br><br><br>
+    <h1><b><?php echo Yii::$app->user->identity->employee_type ?></b></h1></center>
 </div>
