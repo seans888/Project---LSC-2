a547734ff5e73ae5143f316831a20337ca61ef03
Jana Marie Gardon <janamarie.gardon@gmail.com>
2016-12-12 04:27:06 +0800

    update profile

--- Application/advanced/frontend/views/site/profile.php
+++ Application/advanced/frontend/views/site/profile.php
@@ -6,30 +6,27 @@ use yii\helpers\Html;
 
 $this->title = 'Profile';
 //$this->params['breadcrumbs'][] = $this->title;
-?><br/>
-
-
+?><br/><br/><br/>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1><br/>
+    <h1><?= Html::encode($this->title) ?></h1>
+	<table border="3" align="center">
+	<tr>
 
-    <h2><b>ID: </b> <?php echo Yii::$app->user->identity->id ?></h2>
-    <div class="col-sm-12">
-        <h3><b>Username: </b> <?php echo Yii::$app->user->identity->username ?></h3>
-    </div>
-
-    <div class="col-sm-4">
-        <h3><b>First name: </b> <?php echo Yii::$app->user->identity->first_name ?></h3>
-    </div>
-
-    <div class="col-sm-4">
-        <h3><b>Middle name: </b> <?php echo Yii::$app->user->identity->middle_name ?></h3>
-    </div>
-
-    <div class="col-sm-4">
-        <h3><b>Last name: </b> <?php echo Yii::$app->user->identity->last_name ?></h3>
-    </div>
-
-    <h2> <?php echo Yii::$app->user->identity->home_address ?><br/><b>Address </b></h2>
-
+    <th width="300px" bgcolor="#B3C6D8"><h3><b>ID: </b></th> <td><h4><?php echo Yii::$app->user->identity->id ?></h4></td>
+	</tr><tr>
+    <th width="300px" bgcolor="#B3C6D8"><h3><b>Username: </b></th> <td><h4><?php echo Yii::$app->user->identity->username ?></h4></th>
+	</tr><tr>
+    <th width="300px" bgcolor="#B3C6D8"><h3><b>First name: </b></th> <td><h4><?php echo Yii::$app->user->identity->first_name ?></h4></th>
+    </tr><tr>
+	<th width="300px" bgcolor="#B3C6D8"><h3><b>Middle name: </b></th> <td><h4><?php echo Yii::$app->user->identity->middle_name ?></h4></th>
+	</tr><tr>
+    <th width="300px" bgcolor="#B3C6D8"><h3><b>Last name: </b></th> <td><h4><?php echo Yii::$app->user->identity->last_name ?></h4></th>
+	</tr><tr>
+	<th width="300px" bgcolor="#B3C6D8"><h3><b>Address: </b></th> <td><h4><?php echo Yii::$app->user->identity->home_address ?></h4></th>
+	</tr></table>
+	<table border="3" align="center" width="600px">
+	<td><h3 align="center"><a href=""><h2>My Schedule</h2></a></td>
+	</tr>
+	</table>
 
 </div>
