45b942e4c917f2df61a0d0cc23516c3b54f3d005
Jana Marie Gardon <janamarie.gardon@gmail.com>
2016-12-14 08:19:09 +0800

    update frontend and backend design

--- Application/advanced/backend/views/attendance/create.php
+++ Application/advanced/backend/views/attendance/create.php
@@ -13,6 +13,15 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="attendance-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
+	 <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+	
+	    <p>
+        <?php
+        if(Yii::$app->user->can('check-attendance')){
+            echo Html::a('Create Attendance', ['create'], ['class' => 'btn btn-success']);
+        }
+        ?>
+    </p>
 
     <?= $this->render('_form', [
         'model' => $model,
